6194b69d38451adb46e0795dbb7f3f368fd2e19c
Antonio Selenzo <antonio.selenzo@gmail.com>
2017-03-12 18:28:49 +0300

    fixes

--- controllers/api.js
+++ controllers/api.js
@@ -4,6 +4,14 @@ var express = require('express'),
     router = express.Router();
 
 router.get('/', function (req, res) {
+    res.set({
+        'Content-Type': 'application/json',
+//        'Content-Length': jsonTestData.length,
+//        'Last-Modified': new Date(),
+//        'Expires': getExpiration(),
+//        'ETag': null
+    });
+
     res.json({
         'status': 'ok'
     });
