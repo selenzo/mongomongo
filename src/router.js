495f301be45b5dd386d2070007517645e1d7951d
Antonio Selenzo <antonio.selenzo@gmail.com>
2017-03-12 17:19:44 +0300

    roter, web

--- src/router.js
+++ src/router.js
@@ -1,12 +1,25 @@
+/*jshint -W117*/
+
+module.exports.Route = function (req, res) {
+    router(req, res, finalhandler(req, res));
+};
+
 var router = require('router')(),
     finalhandler = require('finalhandler');
 
-module.exports.Route = function (req, res) {
+var web = router.route('/web/');
 
-    router(req, res, finalhandler(req, res))
-}
+web.get(function (req, res) {
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+    res.end('Web')
+});
 
 router.get('/', function (req, res) {
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
-    res.end('Hello World!')
-})
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    res.end('Hello World!');
+});
+
+router.get('/:id', function (req, res, next) {
+    console.log(1231);
+    res.end();
+});
