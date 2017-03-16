292b0c53d91ee02af96c28d1066b98a5ee34fa3e
Antonio Selenzo <antonio.selenzo@gmail.com>
2017-03-16 18:14:31 +0300

    add mongo

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,27 @@
 /* jshint -W117*/
 var gulp = require('gulp'),
-    nodemon = require('gulp-nodemon');
+    nodemon = require('gulp-nodemon'),
+    exec = require('child_process').exec;
 
+gulp.task('start-mongodb', runCommand('mongod --dbpath ./data --journal --storageEngine=mmapv1'));
+//FIXME: hardcore db terminate
+gulp.task('stop-mongodb', runCommand('mongo admin --eval "db.shutdownServer();"'));
 
-gulp.task('start', function () {
-    var stream = nodemon({
-            script: 'server.js',
-            ext: 'js',
-            ignore: ['node_modules/'],
-            tasks: []
-        });
+gulp.task('start-server', function () {
+    nodemon({
+        script: 'server.js',
+        ext: 'js',
+        ignore: ['node_modules/', 'data/'],
+        tasks: []
+    });
 });
+
+function runCommand(command) {
+    return function (cb) {
+        exec(command, function (err, stdout, stderr) {
+            console.log(stdout);
+            console.log(stderr);
+            cb(err);
+        });
+    };
+}
