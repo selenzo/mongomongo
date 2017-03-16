34d9a5b192635def6a8829159a3cfdb8bf92bdeb
Antonio Selenzo <antonio.selenzo@gmail.com>
2017-03-16 18:30:38 +0300

    gulp-mocha tests

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,11 +1,8 @@
 /* jshint -W117*/
 var gulp = require('gulp'),
     nodemon = require('gulp-nodemon'),
-    exec = require('child_process').exec;
-
-gulp.task('start-mongodb', runCommand('mongod --dbpath ./data --journal --storageEngine=mmapv1'));
-//FIXME: hardcore db terminate
-gulp.task('stop-mongodb', runCommand('mongo admin --eval "db.shutdownServer();"'));
+    exec = require('child_process').exec,
+    mocha = require('gulp-mocha');
 
 gulp.task('start-server', function () {
     nodemon({
@@ -15,6 +12,17 @@ gulp.task('start-server', function () {
         tasks: []
     });
 });
+
+gulp.task('test', function () {
+    gulp.src('./test/*.js', {
+            read: false
+        }) 
+        .pipe(mocha());
+});
+
+gulp.task('start-mongodb', runCommand('mongod --dbpath ./data --journal --storageEngine=mmapv1'));
+//FIXME: hardcore db terminate
+gulp.task('stop-mongodb', runCommand('mongo admin --eval "db.shutdownServer();"'));
 
 function runCommand(command) {
     return function (cb) {
