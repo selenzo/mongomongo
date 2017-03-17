4306e018886842554869cf768feb85fcf05fb7ac
Antonio Selenzo <antonio.selenzo@gmail.com>
2017-03-17 13:38:40 +0300

    add monk mongodb connection

--- controllers/api.js
+++ controllers/api.js
@@ -2,19 +2,37 @@
 var express = require('express'),
     router = express.Router();
 
-router.get('/', function (req, res) {
+router.use(function (req, res, next) {
+    console.info("REQUEST!");
     res.set({
-//TODO: set params
+        //TODO: set params
         'Content-Type': 'application/json',
-//        'Content-Length': jsonTestData.length,
-//        'Last-Modified': new Date(),
-//        'Expires': getExpiration(),
-//        'ETag': null
+        //        'Content-Length': jsonTestData.length,
+        //        'Last-Modified': new Date(),
+        //        'Expires': getExpiration(),
+        //        'ETag': null
     });
+    next();
+});
 
+router.get('/', function (req, res) {
     res.json({
         'status': 'ok'
     });
 });
 
+
+router.get('/db', function (req, res) {
+    req.db.get('test').find({}, {}).then(function (data) {
+        console.log(data.length);
+        var answer = {
+            'status': 'ok',
+            'data': data
+        }
+        res.set({
+            'Content-Length': answer.length
+        });
+        res.json(answer);
+    });
+});
 module.exports = router;
---
+++ controllers/db.js
@@ -0,0 +1,15 @@
+/*jshint -W117*/
+
+var express = require('express'),
+    router = express.Router(),
+    config = require('config-yml'),
+    db = require('monk')(config.db.connectionString);
+
+router.use(function (req, res, next) {
+    req.db = db;
+    next();
+});
+
+module.exports = router;
+
+// use mongomongo; db.createUser({user: "mongomongo",pwd: "test",roles: [{role: "readWrite",db: "mongomongo"}]})
