45baf6e8a2ed3d9b10e477439e0ae2d0d906976d
Antonio Selenzo <antonio.selenzo@gmail.com>
2017-03-19 18:28:28 +0300

    cookie tests

--- controllers/index.js
+++ controllers/index.js
@@ -4,11 +4,22 @@ var express = require('express'),
 
 router.use('/api', require('./api'));
 
-router.get('/', function (req, res) {
-    res.send('Home page');
+router.get('/test', function (req, res) {
+    req.session.asd = (req.session.asd || 0) + 1 ;
+
+    console.log((req.session.asd || 0) + 1 );
+
+    console.log('Cookies: ', req.cookie);
+
+    console.log(req.session);
+    console.log("123");
+//    req.session = null;
+
+    res.send('test page ' + req.session.asd);
 });
 
 router.get('/about', function (req, res) {
+
     res.send('Learn about us');
 });
 
